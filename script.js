de7e0e7f5cb4fa556fcfafcc1d69c57a851afa9f
djns <130430285+DDJCS50@users.noreply.github.com>
2023-06-05 10:17:57 -0700

    Add functions
    Added basic arithmetic to the outlined functions. Added operate function
    that calls the basic math functions based on what operator was used.
    Linked css and js files to html file.

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function add(a, b) {
-
+    return a + b;
 }
 
 function subtract(a, b) {
-
+    return a - b;
 }
 
 function multiply(a, b) {
-
+    return a * b;
 }
 
 function divide(a, b) {
+    return a / b;
+}
 
+let num1;
+let operator;
+let num2;
+
+function operate(firstInputNum, inputOperator, secondInputNum) {
+    if (inputOperator == '+') {
+        return add(firstInputNum, secondInputNum);
+    } else if (inputOperator == '-') {
+        return subtract(firstInputNum, secondInputNum);
+    } else if (inputOperator == '*') {
+        return multiply(firstInputNum, secondInputNum);
+    } else if (inputOperator == '/') {
+        return divide(firstInputNum, secondInputNum);
+    }
 }
