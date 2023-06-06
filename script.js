ec27ee7c58e9dd06435453e955f9286a43a3c85c
djns <130430285+DDJCS50@users.noreply.github.com>
2023-06-06 11:29:56 -0700

    Add button display funcs
    Added id to main display of calculator for targeting with styling.
    Altered styling of display container so that the content would wrap
    instead of expanding the width of the calculator. Added functions
    to the buttons in the js file so that the display will update each
    time a button is pressed with its corresponding value

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function divide(a, b) {
 let num1;
 let operator;
 let num2;
-let displayValue = 1;
+let displayValue = 0;
 
 let calcDisplay = document.querySelector('#display');
 calcDisplay.innerText = displayValue;
@@ -37,25 +37,112 @@ function operate(firstInputNum, inputOperator, secondInputNum) {
 let clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', function(event) {
     event.stopPropagation();
-    displayValue = 0;
-    calcDisplay.innerText = displayValue;
+    totalDisplay.textContent = '';
+    populateDisplay(0);
 });
 let divideBtn = document.querySelector('#divideBtn');
+divideBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    populateDisplay('\xa0/\xa0');
+    // call operate function using inputted data !!!
+});
 let sevenBtn = document.querySelector('#sevenBtn');
+sevenBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    populateDisplay('7');
+});
 let eightBtn = document.querySelector('#eightBtn');
+eightBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('8');
+});
 let nineBtn = document.querySelector('#nineBtn');
+nineBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('9');
+});
 let multiplyBtn = document.querySelector('#multiplyBtn');
+multiplyBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    populateDisplay('\xa0x\xa0');
+    // call operate function using inputted data !!!
+});
 let fourBtn = document.querySelector('#fourBtn');
+fourBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('4');
+});
 let fiveBtn = document.querySelector('#fiveBtn');
+fiveBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('5');
+});
 let sixBtn = document.querySelector('#sixBtn');
+sixBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('6');
+});
 let subtractBtn = document.querySelector('#subtractBtn');
+subtractBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    populateDisplay('\xa0-\xa0');
+    // call operate function using inputted data !!!
+});
 let oneBtn = document.querySelector('#oneBtn');
+oneBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('1');
+});
 let twoBtn = document.querySelector('#twoBtn');
+twoBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('2');
+});
 let threeBtn = document.querySelector('#threeBtn');
+threeBtn.addEventListener('click', function(event) {
+    if (totalDisplay.textContent == 0) totalDisplay.textContent = '';
+    event.stopPropagation();
+    populateDisplay('3');
+});
 let addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    populateDisplay('\xa0+\xa0');
+    // call operate function using inputted data !!!
+});
 let zeroBtn = document.querySelector('#zeroBtn');
+zeroBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    populateDisplay('0');
+});
 let equalsBtn = document.querySelector('#equalsBtn');
+equalsBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    // call operate function using inputted data !!!
+});
+
+let totalDisplay = document.querySelector('#displayContainer');
 
 function populateDisplay(btn) {
+    let newDisplayElement = document.createElement('p');
+    newDisplayElement.innerText = btn;
+    newDisplayElement.style.display = 'flex';
+    newDisplayElement.style.justifyContent = 'center';
+    newDisplayElement.style.flexDirection = 'column';
+    newDisplayElement.style.alignItems = 'flex-end';
+    newDisplayElement.style.fontWeight = 'bolder';
+    newDisplayElement.style.fontSize = '48px';
+    totalDisplay.appendChild(newDisplayElement);
+}
 
+function addToDisplay(btn) {
+    // currentP.concat(newText) -> Add concat functionality instead of appending endless p's !!!!!!
 }
